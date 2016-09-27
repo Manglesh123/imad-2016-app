d9fad0937178ee8302a723eacf0c6a767a298bcc
Manglesh Saurabh <mangleshsaurabh@gmail.com>
2016-09-27 14:49:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone', function(req,res){
-    res.send('Article one has not yet been published')
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     
 });
 app.get('/ui/style.css', function (req, res) {
